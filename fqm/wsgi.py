63b43433f3cc89867f85195bd5182c5b23f24cc4
Ephergent <ephergent@gmail.com>
2023-12-03 13:18:11 -0800

    remove newrelic

--- fqm/wsgi.py
+++ fqm/wsgi.py
@@ -6,9 +6,6 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
-import newrelic.agent
-newrelic.agent.initialize('./newrelic.ini')
-
 import os
 
 from django.core.wsgi import get_wsgi_application
